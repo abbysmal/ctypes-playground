42630fad6b4010e30ccd151b68e30a3b15c52db5
Enguerrand Decorne <decorne.en@gmail.com>
2016-03-22 20:30:23 +0000

    Change value naming

--- expat/expat.ml
+++ expat/expat.ml
@@ -24,29 +24,25 @@ let ptr_string_opt_to_list ptr =
 let set_element_handler =
   let aux = foreign "XML_SetElementHandler" (xmlparser @-> funptr start_element_handler_t @-> funptr end_element_handler_t @-> returning void) in
   (fun parser (start : start_handler) (ed : end_handler) ->
-   let start' p s a =
-     let a = ptr_string_opt_to_list a in
-     start p s a in
-   let end' p s =
-     let s = s in
-     ed p s in
-   aux parser start' end'
+   let start' parser element attributes =
+     let attributes' = ptr_string_opt_to_list attributes in
+     start parser element attributes' in
+   aux parser start' ed
   )
 
 let set_character_data_handler =
   let aux = foreign "XML_SetCharacterDataHandler" (xmlparser @-> funptr character_data_handler_t @-> returning void) in
   (fun parser handler ->
-   let handler' p c l =
-     let c = CArray.from_ptr c l |> CArray.to_list |> List.map Char.escaped |> List.fold_left (^) "" in
-     handler p c in
+   let handler' parser content length =
+     let content' = CArray.from_ptr content length |> CArray.to_list |> List.map Char.escaped |> List.fold_left (^) "" in
+     handler parser content' in
    aux parser handler')
 
 let parse =
   let aux = foreign "XML_Parse" (xmlparser @-> string @-> int @-> int @-> returning void) in
   (fun parser buffer ->
    let len = String.length buffer in
-   let i = 0 in
-   aux parser buffer len i)
+   aux parser buffer len 0)
 
 let parser_create =
   let aux = foreign "XML_ParserCreate" (ptr void @-> returning xmlparser) in
